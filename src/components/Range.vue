c61439a11d3ff46ffd9fa897d8492a3aa3f7f01d
Ingrid <ingridgti08@hotmail.com>
2022-08-15 19:42:26 -0300

    cria componente range e adiciona na home

---
+++ src/components/Range.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <label for="customRange1">{{ label }}</label>
+    <span>{{ numberRange }}</span>
+    <div class="d-flex">
+      <span class="me-3">{{ numberMin }}</span>
+      <input
+        type="range"
+        id="customRange1"
+        :value="value"
+        @input="updateInput($event.target.value)"
+      />
+      <span class="ms-3">{{ numberMax }}</span>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Range",
+  props: {
+    label: {
+      type: String,
+    },
+    value: {
+      type: String,
+    },
+    numberRange: {
+      type: String,
+    },
+    numberMin: {
+      type: String,
+    },
+    numberMax: {
+      type: String,
+    },
+  },
+
+  setup(props, { emit }) {
+    const updateInput = (value) => {
+      emit("onInput", value);
+    };
+    return {
+      updateInput,
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,20 @@
     <div class="d-flex flex-wrap justify-content-center mt-5">
       <h1>Le/Tip</h1>
     </div>
-    <Input label="Valor" :icon="icon" />
+    <div class="ms-5">
+      <Input label="Valor" :icon="icon" />
+      <Range label="Gorjeta" numberMin="10" numberMax="20" class="mt-3" />
+      <Range label="Pessoas" numberMin="2" numberMax="6" />
+    </div>
   </div>
 </template>
 
 <script>
 import Input from "../components/Input.vue";
+import Range from "../components/Range.vue";
+
 export default {
-  components: { Input },
+  components: { Input, Range },
   setup() {},
 };
 </script>
