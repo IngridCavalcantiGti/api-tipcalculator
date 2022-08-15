db18f7eec8b29c6b996714fad8609f59d1a49eb1
Ingrid <ingridgti08@hotmail.com>
2022-08-15 19:14:22 -0300

    cria componente imput e adiciona bootstrap

---
+++ src/components/Input.vue
@@ -0,0 +1,45 @@
+<template>
+  <label class="d-flex align-items-center">
+    <span class="label me-2"> {{ label }}</span>
+    <i :class="`bi bi-currency-${icon}`"></i>
+    <div>
+      <input
+        class="p-2"
+        type="text"
+        @input="updateInput($event.target.value)"
+        :value="value"
+      />
+    </div>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    label: {
+      type: String,
+    },
+    value: {
+      type: String,
+    },
+    icon: {
+      type: String,
+      default: "dollar",
+    },
+  },
+  setup(props, { emit }) {
+    const updateInput = (value) => {
+      emit("onInput", value);
+    };
+    return {
+      updateInput,
+    };
+  },
+};
+</script>
+<style scoped>
+.label {
+  font-size: 1rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="home">
-    <span>teste</span>
+  <div>
+    <div class="d-flex flex-wrap justify-content-center mt-5">
+      <h1>Le/Tip</h1>
+    </div>
+    <Input label="Valor" :icon="icon" />
   </div>
 </template>
+
+<script>
+import Input from "../components/Input.vue";
+export default {
+  components: { Input },
+  setup() {},
+};
+</script>
 
