d40115a79afdb318e6e707973b0f2620ab613e2a
Ingrid <ingridgti08@hotmail.com>
2022-08-16 13:50:29 -0300

    cria e adiciona componente switch

---
+++ src/components/Switch.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-flex align-items-center">
+    <label class="form-check-label">Eur</label>
+    <div class="form-control-lg form-switch">
+      <input class="form-check-input" type="checkbox" />
+    </div>
+    <label class="form-check-label">USD</label>
+  </div>
+</template>
+<style scoped>
+.form-switch .form-check-input {
+  width: 3em;
+  border-radius: 1px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,13 @@
     <div class="d-flex flex-wrap justify-content-center mt-5">
       <h1>Le/Tip</h1>
     </div>
-    <div class="ms-5">
-      <Input label="Valor" :icon="icon" />
-      <Range label="Gorjeta" numberMin="10" numberMax="20" class="mt-3" />
-      <Range label="Pessoas" numberMin="2" numberMax="6" />
+    <div class="ms-5 d-flex flex-wrap justify-content-center">
+      <div>
+        <Switch></Switch>
+        <Input label="Valor" :icon="icon" />
+        <Range label="Gorjeta" numberMin="10" numberMax="20" class="mt-3" />
+        <Range label="Pessoas" numberMin="2" numberMax="6" />
+      </div>
     </div>
   </div>
 </template>
@@ -14,9 +17,10 @@
 <script>
 import Input from "../components/Input.vue";
 import Range from "../components/Range.vue";
+import Switch from "../components/Switch.vue";
 
 export default {
-  components: { Input, Range },
+  components: { Input, Range, Switch },
   setup() {},
 };
 </script>
